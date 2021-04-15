d7339955119d40a00f20227bc035ca0b2efef668
Zac <zilson19878@gmail.com>
2021-04-14 23:58:35 -0500

    added header component -WIP

---
+++ src/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<main>
+  <header>
+    <div class="search-bar">
+      <input type="text" class="search" />
+    </div>
+    <i class="fas fa-hat-wizard" />
+  </header>
+</main>
+
+<style>
+  main {
+    left: 0;
+  }
+  header {
+    display: flex;
+    flex-direction: row;
+    width: 100vw;
+    height: 5vh;
+    left: 0;
+    background-color: #1e2022;
+
+    align-items: center;
+    align-content: center;
+  }
+</style>
