b81a9645496c417e03544325a22b1042ac9d24a4
Santiago <santi1795@hotmail.com>
2021-04-15 12:16:23 -0500

    Create Footer.svelte
    Created footer

---
+++ src/Footer.svelte
@@ -0,0 +1,20 @@
+<footer>
+	<p>A Creation by Bacari, Zac, Miguel and Santiago!</p>
+	<p>Visit the <a href="https://trello.com/b/Sfg4uYY6/mk1">Trello Board</a> to check out our progress.</p>
+</footer>
+
+<style>
+	footer {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+        background-color: grey;
+    }
+
+	p {
+		color: #06fd1a;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
