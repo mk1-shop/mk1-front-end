61b19212d55b2b79868c22a257aae5547f6e74d5
xamthor <miguel@mhernandez.me>
2021-04-17 04:47:38 -0500

    nav style tweaks

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,10 +1,15 @@
 <script type="text/javascript">
 	import { link } from "svelte-spa-router";
 </script>
+
+<div class="text-center mb-4"> 
+	
 <a href="/" use:link class="inline-block px-5 m-3">Home</a>
 <a href="/contact" use:link class="inline-block px-5 m-3">Contact</a>
 <a href="/about" use:link class="inline-block px-5 m-3">About</a>
 <a href="/admin" use:link class="inline-block px-5 m-3">Admin</a>
-
-
 <hr />
+
+</div>
+
+
