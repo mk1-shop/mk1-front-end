0ad7cd3085b1fe9ecdb34943b5bc9196fe35a153
xamthor <miguel@mhernandez.me>
2021-04-17 05:08:18 -0500

    view product ui

--- src/pages/ViewProduct.svelte
+++ src/pages/ViewProduct.svelte
@@ -19,21 +19,20 @@
 </script>
 
 {#if product}
-  <div class="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden">
-    <div class="px-4 py-2">
-      <h1
-        class="text-gray-900 text-center font-bold text-lg uppercase h-8 overflow-hidden"
-      >
-        {product.name}
-      </h1>
-    </div>
-    <img class="h-56 w-full object-cover mt-2" src={product.image} alt="" />
-    <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
-      <h1 class="text-gray-200 font-bold text-xl">${product.price}</h1>
-      <button
-        class="px-3 py-1 bg-gray-200 text-sm text-gray-900 font-semibold rounded"
-        on:click={addCart(product)}>Add to cart</button
-      >
-    </div>
-  </div>
+	<div class="container mx-auto grid grid-cols-3 gap-12 my-12">
+		<div class="">
+			<img class="h-96 w-full object-contain mt-2" src={product.image} alt="" />
+		</div>
+
+		<div class="col-span-2 text-center">
+			<h1 class="text-gray-900 text-center font-bold text-lg uppercase mb-12">
+				{product.name}
+			</h1>
+		   	<p class="mx-auto mb-10 text-justify">{product.description}</p>
+		    <p class="text-gray-800 font-bold text-xl mb-4">${product.price}</p>
+		     <button class="px-3 py-1 bg-gray-800 text-sm text-white font-semibold rounded" on:click={addCart(product)}>Add to cart</button>
+		  </div>
+		
+	</div>
+ 
 {/if}
