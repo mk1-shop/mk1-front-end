09802985a276fe175658ca6af0479d968e00f29a
Zac <zilson19878@gmail.com>
2021-04-17 20:39:01 -0500

    misc

--- src/components/productCard.svelte
+++ src/components/productCard.svelte
@@ -1,30 +1,29 @@
-
 <script>
-    export let product;
-    import { link } from "svelte-spa-router";
-    import cart from "../store/cart";
+  export let product;
+  import { link } from "svelte-spa-router";
+  import cart from "../store/cart";
 
-    const addCart = (product) => {
-        cart.addProduct(product, "add");
+  const addCart = (product) => {
+    cart.addProduct(product, "add");
   };
 </script>
 
 <div class="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden">
-    <div class="px-4 py-2">
-      <a href="/product/{product.id}" use:link class="inline-block px-10 m-2">
-        <h1
-          class="text-gray-900 text-center font-bold text-lg uppercase h-8 overflow-hidden"
-        >
-          {product.name}
-        </h1></a
+  <div class="px-4 py-2">
+    <a href="/product/{product.id}" use:link class="inline-block px-10 m-2">
+      <h1
+        class="text-gray-900 text-center font-bold text-lg uppercase h-8 overflow-hidden"
       >
-    </div>
-    <img class="h-56 w-full object-cover mt-2" src={product.image} alt="" />
-    <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
-      <h1 class="text-gray-200 font-bold text-xl">${product.price}</h1>
-      <button
-        class="px-3 py-1 bg-gray-200 text-sm text-gray-900 font-semibold rounded"
-        on:click={addCart(product)}>Add to cart</button
-      >
-    </div>
+        {product.name}
+      </h1></a
+    >
   </div>
+  <img class="h-56 w-full object-cover mt-2" src={product.image} alt="" />
+  <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
+    <h1 class="text-gray-200 font-bold text-xl">${product.price}</h1>
+    <button
+      class="px-3 py-1 bg-gray-200 text-sm text-gray-900 font-semibold rounded"
+      on:click={addCart(product)}>Add to cart</button
+    >
+  </div>
+</div>
