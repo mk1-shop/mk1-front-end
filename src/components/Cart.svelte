c6efee75efb3d0e45a36db5c8f7fd5a541c869ee
xamthor <miguel@mhernandez.me>
2021-04-18 20:41:06 -0500

    Cart Notification Component

--- src/components/Cart.svelte
+++ src/components/Cart.svelte
@@ -12,10 +12,30 @@
 	const toggleCart = () =>{
 		showCart = !showCart
 	}
+
+
+	import { draw } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import { onMount } from "svelte";
+
+	onMount(async () => {
+		setTimeout(() => {
+			condition = !condition
+	  		
+	    }, 1500);
+	});
+
+	let condition = false;
+
+
 </script>
 
 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mx-4 text-white cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" on:click={toggleCart}>
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+  
+{#if condition}
+  <path transition:draw="{{duration: 12000, delay: 0, easing: quintOut}}" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+{/if}
+
 </svg>
 
 {#if showCart}
