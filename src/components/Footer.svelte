4408934b3b74d4943901a04b4ff3af797a1ae69c
Santi <70606628+santiareh@users.noreply.github.com>
2021-04-18 19:33:54 -0500

    Footer color and link

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,7 +3,7 @@
 <footer class="py-4">
   <p>A Creation by Bacari, Zac, Miguel and Santiago!</p>
   <p>
-    Visit the <a href="https://trello.com/b/Sfg4uYY6/mk1">Trello Board</a> to check
+    Visit the <a class= "hover:underline" href="https://trello.com/b/Sfg4uYY6/mk1">Trello Board</a> to check
     out our progress.
   </p>
 </footer>
@@ -21,5 +21,9 @@
     color: #fff;
     font-size: 1em;
     font-weight: 100;
+  }
+
+  a{
+    color: blue;
   }
 </style>
