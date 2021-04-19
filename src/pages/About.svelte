734fb6c093823cc6a70e212565bf8b6f2eeea318
xamthor <miguel@mhernandez.me>
2021-04-19 13:33:07 -0500

    About Page

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,1 +1,18 @@
-<h1>About Page</h1>
+
+
+<div class="container mx-auto">
+	
+<h1 class="text-center">About</h1>
+
+
+
+<div class="grid grid-cols-2 gap-6 m-12">
+	
+<a href="https://github.com/mk1-shop/mk1-front-end" class="justify-self-end "><img src="https://gh-card.dev/repos/mk1-shop/mk1-front-end.svg"></a>
+
+<a href="https://github.com/mk1-shop/mk1-back-end" class="justify-self-start"><img src="https://gh-card.dev/repos/mk1-shop/mk1-back-end.svg?fullname="></a>
+
+</div>
+
+
+</div>
